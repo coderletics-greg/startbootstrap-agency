52dd1527cbae15471179b4ebb801f03044921940
coderletics-greg <greg.aubert@yahoo.co.uk>
2014-12-12 22:09:18 +0000

    features and video pages done

--- js/cbpAnimatedHeader_features.js
+++ js/cbpAnimatedHeader_features.js
@@ -13,14 +13,13 @@ var cbpAnimatedHeader = (function() {
 	var docElem = document.documentElement,
 		header = document.querySelector( '.navbar-default' ),
 		didScroll = false,
-		changeHeaderOn = 180;
+		changeHeaderOn = 80;
 
 	function init() {
 		window.addEventListener( 'scroll', function( event ) {
 			if( !didScroll ) {
 				didScroll = true;
 				setTimeout( scrollPage, 1 );
-				// was 250 before
 			}
 		}, false );
 	}
